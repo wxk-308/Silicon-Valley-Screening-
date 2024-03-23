2a49699cf5a4a8eae07bbd5d9a866547e3be14da
hw_008619857277621_01 <2442564348@qq.com>
2024-03-23 16:05:38 +0800

    提交初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="first">hhhhh</div>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { useCounterStore } from '@/stores/index'
+</script>
+
+<style lang="scss" scoped>
+.first {
+  width: 600px;
+  height: 400px;
+  background: blue;
+}
+</style>
